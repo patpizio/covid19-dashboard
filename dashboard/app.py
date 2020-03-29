35ce4c9b562e1174e363ac831e9e532505156c21
patpizio <15801338+patpizio@users.noreply.github.com>
2020-03-29 23:50:10 +0100

    add dropdown cases-deaths

--- dashboard/app.py
+++ dashboard/app.py
@@ -13,14 +13,21 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 min_cases = 60
 cumul = prepare_data()
+available_vars = cumul['show'].unique()
 
 app.layout = html.Div([
     html.H1(
-        'Covid-19 cases worldwide trend',
+        'The numbers of Covid-19',
         style={
             'textAlign': 'center',
             # 'color': colors['text']
         }
+    ),
+
+    dcc.Dropdown(
+        id='show-variable',
+        options=[{'label': i, 'value': i} for i in available_vars],
+        value='cases'
     ),
 
     dcc.Graph(
@@ -33,10 +40,11 @@ app.layout = html.Div([
 
 @app.callback(
     Output('cumul-trends', 'figure'),
-    [Input('input-min_cases', 'value')])
-def update_alignment(min_cases):
+    [Input('input-min_cases', 'value'),
+     Input('show-variable', 'value')])
+def update_alignment(min_cases, chosen_var):
     cumul_aligned = cumul[cumul['value'] > min_cases]
-    fig = px.line(cumul_aligned[cumul_aligned['show'] == 'deaths'], y='value', color='countriesAndTerritories', log_y=False)
+    fig = px.line(cumul_aligned[cumul_aligned['show'] == chosen_var], y='value', color='countriesAndTerritories', log_y=False)
     fig.update_layout(title='Cases', xaxis_title='Days from ' + str(min_cases) + 'th case')
     return fig
 
